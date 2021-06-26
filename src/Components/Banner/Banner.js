3571e335e2952ee18a8f977029518f92986adb81
Rasha Faheem <64769011+rashafaheem@users.noreply.github.com>
2021-06-26 00:20:07 -0700

    Dynamic poster

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,25 +1,27 @@
-import React, {useEffect} from 'react'
-import {API_KEY} from '../../constants/constants'
+import React, {useEffect, useState} from 'react'
+import {API_KEY,imageUrl} from '../../constants/constants'
 import axios from '../../axios'
 import './Banner.css'
 
 function Banner() {
+    const [movie, setMovie] = useState()
     useEffect(() => {
         axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=> {
             console.log(response.data.results[0])
+            setMovie(response.data.results[4])
         })
     }, [])
     return (
-        <div className='banner'>
+        <div 
+        style = {{backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path : ""})`}}
+        className='banner'>
             <div className='content'>
-                <h1 className='title'>Movie Name</h1>
+                <h1 className='title'>{movie ? movie.title : ""}</h1>
                 <div className='banner_buttons'>
                     <button className='button'>Play</button>
                     <button className='button'>My List</button>
                 </div>
-                <h1 className='description'>Netflix is a subscription-based streaming service that allows our members to watch TV shows and movies without commercials on an internet-connected device. 
-                You can also download TV shows and movies to your iOS, Android, or Windows 10 device and watch without an internet connection. 
-                </h1>
+                <h1 className='description'>{movie ? movie.overview : ""}</h1>
             </div>  
             <div className='fade_bottom'></div>
         </div>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,3 @@
 export const baseUrl = 'https://api.themoviedb.org/3';
 export const API_KEY = 'ffd25e8ac2bea9aef7f6bdde6405c5d4';
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
