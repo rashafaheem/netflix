cda09c97c086c3007209fe09cb5fdd6c22798fad
Rasha Faheem <64769011+rashafaheem@users.noreply.github.com>
2021-06-28 03:19:01 -0700

    Action

--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -1,22 +1,22 @@
 import React,{useEffect,useState} from 'react'
 import './RowPost.css'
 import axios from '../../axios'
-import {API_KEY,imageUrl} from '../../constants/constants'
+import {imageUrl} from '../../constants/constants'
 
-function RowPost() {
+function RowPost(props) {
     const [movies, setmovies] = useState([])
     useEffect(() => {
-       axios.get(`discover/tv?api_key=${API_KEY}&with_networks=213`).then(response =>{
+       axios.get(props.url).then(response =>{
            console.log(response.data)
            setmovies(response.data.results)
        })
     }, [])
     return (
         <div className='row'>
-            <h2>Netflix Originals</h2>
+            <h2>{props.title}</h2>
             <div className='posters'>
                 {movies.map((obj)=>
-                <img className='poster' alt='poster' src={`${imageUrl+obj.backdrop_path}`}></img>
+                <img className={props.isSmall ? 'smallPoster':'poster'} alt='poster' src={`${imageUrl+obj.backdrop_path}`}></img>
                 )}
                 
             </div>
